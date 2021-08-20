7a520232e42d004a76c95449a8c239e8fdbaa407
John Waczak <john.louis.waczak@gmail.com>
2021-08-20 17:50:19 -0500

    added central node plot

--- LabCentralNode/app.jl
+++ LabCentralNode/app.jl
@@ -12,7 +12,13 @@ binBoundariesLow  = [0.35,.46,.66,1,1.3,1.7,2.3,3.0,4.0,5.2,6.5,8,10,12,14,16,18
 binCenters = 0.5 .* (binBoundariesHigh .+ binBoundariesLow)
 
 
+binBoundariesHigh2 = [0.1, 0.3, 0.5, 1.0, 2.5, 5.0, 10.0];
+binBoundariesLow2  = [0.0, 0.1, 0.3, 0.5, 1.0, 2.5, 5.0];
+binCenters2 = 0.5 .* (binBoundariesHigh2 .+ binBoundariesLow2)
+
+
 # load in some test data to play with
+IPS7100()
 APDS9002()
 BME280()
 GUV001()
@@ -51,6 +57,33 @@ function plotPM()
                   marker_size = 5,
                   )
 
+    pm1_ips = scatter(;
+                    x = sensors[:IPS7100][:dt],
+                    y = sensors[:IPS7100][:pm1_0],
+                    mode = "lines+markers",
+                    name = "PM 1 IPS",
+                    marker_color = :cyan,
+                    marker_size = 5,
+                  )
+
+    pm2_5_ips = scatter(;
+                        x = sensors[:IPS7100][:dt],
+                        y = sensors[:IPS7100][:pm2_5],
+                        mode = "lines+markers",
+                        name = "PM 2.5 IPS",
+                        marker_color = :black,
+                        marker_size = 5,
+                    )
+
+    pm10_ips = scatter(;
+                       x = sensors[:IPS7100][:dt],
+                       y = sensors[:IPS7100][:pm10_0],
+                   mode = "lines+markers",
+                   name = "PM 10 IPS",
+                   marker_color = :purple,
+                   marker_size = 5,
+                   )
+
 
 
     layout = Layout(;
@@ -60,7 +93,7 @@ function plotPM()
                     plot_bgcolor = :transparent,
                     paper_bgcolor = :transparent,
                     )
-    plot([pm1, pm2_5, pm10], layout)
+    plot([pm1, pm2_5, pm10, pm1_ips, pm2_5_ips, pm10_ips], layout)
 end
 
 
@@ -91,7 +124,7 @@ function plotContour()
                    )
 
     layout = Layout(;
-                    title = "Particle Size Distribution",
+                    title = "Particle Size Distribution (OPCN3)",
                     xaxis_title = "time",
                     yaxis_title = "Particle Radius [μm]",
                     yaxis_type = "log",
@@ -103,11 +136,33 @@ end
 
 
 
+function plotContour2()
+    data = contour(;
+                   x = sensors[:IPS7100][:dt],
+                   y = binCenters2,
+                   z = log10.(hcat(sensors[:IPS7100][:bins]...) .+ 1.0 ),  # add 1 to each count so we don't have issues with log(0)
+                   colorscale = "Jet",
+                   ncontours = 100,
+                   contours_showlines = false,
+                   )
+
+    layout = Layout(;
+                    title = "Particle Size Distribution (IPS7100 sensor)",
+                    xaxis_title = "time",
+                    yaxis_title = "Particle Radius [μm]",
+                    yaxis_type = "log",
+                    plot_bgcolor = :transparent,
+                    paper_bgcolor = :transparent,
+                    )
+    plot(data, layout)
+
+end
 
 
 
 function update_pm()
     # load in some test data to play with
+    IPS7100()
     APDS9002()
     BME280()
     GUV001()
@@ -124,6 +179,11 @@ function update_contour()
 end
 
 
+function update_contour2()
+    plotContour2()
+end
+
+
 
 #app_color = Dict("graph_bg"=>"#082255", "graph_line"=>"#007ACE")
 
@@ -131,7 +191,7 @@ app = dash()
 
 app.layout = html_div() do
     html_h1(
-        "MINTS Live Dashboards: AV Air Package",
+        "MINTS Live Dashboards: Central Node Air Package",
         style=Dict("color"=>:black, "textAlign"=>"center"),
     ),
     html_div(
@@ -144,7 +204,10 @@ app.layout = html_div() do
                 id="size-contour",
                 figure = plotContour()
             ),
-
+            dcc_graph(
+                id="size-contour2",
+                figure = plotContour2()
+            ),
             dcc_interval(
                 id="interval-component",
                 interval = 250, # 250 miliseconds
@@ -164,6 +227,11 @@ end
 
 callback!(app, Output("size-contour", "figure"), Input("interval-component", "n_intervals")) do n
     update_contour()
+end
+
+
+callback!(app, Output("size-contour2", "figure"), Input("interval-component", "n_intervals")) do n
+    update_contour2()
 end
 
 
---
+++ LabCentralNode/json_read.jl
@@ -0,0 +1,490 @@
+using JSON
+using Dates
+
+
+dateformat = DateFormat("yyyy-mm-dd HH:MM:SS")
+Npoints = 1000
+basepath = "/home/teamlary/mintsData/rawMQTT/001e06373996"
+
+
+sensors = Dict(:IPS7100 => Dict(:path => joinpath(basepath, "IPS7100.json"),
+                                :dt => DateTime[],
+                                :pm0_1 => Float64[],
+                                :pm0_3 => Float64[],
+                                :pm0_5 => Float64[],
+                                :pm1_0 => Float64[],
+                                :pm2_5 => Float64[],
+                                :pm5_0 => Float64[],
+                                :pm10_0 => Float64[],
+                                :bins => Vector{Int}[],
+                                ),
+               :APDS9002 => Dict(:path => joinpath(basepath, "APDS9002.json"),
+                                 :luminance => Float64[],
+                                 :dt => DateTime[],
+                                 ),
+               :BME280 => Dict(:path => joinpath(basepath, "BME280.json"),
+                               :pressure => Float64[],
+                               :humidity => Float64[],
+                               :temperature => Float64[],
+                               :dt => DateTime[],
+                               ),
+               :GUV001 => Dict(:path => joinpath(basepath, "GUV001.json"),
+                               :uvLevel => Float64[],
+                               :dt => DateTime[],
+                               ),
+               :MGS001 => Dict(:path => joinpath(basepath, "MGS001.json"),
+                               :c2h5oh => Float64[],
+                               :nh3 => Float64[],
+                               :c4h10 => Float64[],
+                               :c3h8 => Float64[],
+                               :ch4 => Float64[],
+                               :co => Float64[],
+                               :h2 => Float64[],
+                               :no2 => Float64[],
+                               :dt => DateTime[],
+                               ),
+               :OPCN3 => Dict(:path => joinpath(basepath, "OPCN3.json"),
+                              :pm1 => Float64[],
+                              :pm2_5 => Float64[],
+                              :pm10 => Float64[],
+                              :bins => Vector{Float64}[],
+                              :dt => DateTime[],
+                              ),
+               :SI114X => Dict(:path =>joinpath(basepath, "SI114X.json"),
+                               :visible => Float64[],
+                               :uv => Float64[],
+                               :ir => Float64[],
+                               :proximity1 => Float64[],
+                               :proximity2 => Float64[],
+                               :proximity3 => Float64[],
+                               :dt => DateTime[],
+                               ),
+              )
+
+
+function IPS7100()
+    res = JSON.parsefile(sensors[:IPS7100][:path])
+
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+
+    if length(sensors[:IPS7100][:dt]) > 0 &&  dt == sensors[:IPS7100][:dt][end]
+        return -1
+    end
+
+
+    if length(sensors[:IPS7100][:dt]) < Npoints
+        push!(sensors[:IPS7100][:dt], dt)
+    else
+        sensors[:IPS7100][:dt] .= circshift(sensors[:IPS7100][:dt], -1)
+        sensors[:IPS7100][:dt][end] = dt
+    end
+
+
+
+
+    if length(sensors[:IPS7100][:pm0_1]) < Npoints
+        push!(sensors[:IPS7100][:pm0_1], parse(Float64, res["pm0_1"]))
+    else
+        sensors[:IPS7100][:pm0_1] .= circshift(sensors[:IPS7100][:pm0_1], -1)
+        sensors[:IPS7100][:pm0_1][end] = parse(Float64, res["pm0_1"])
+    end
+
+
+    if length(sensors[:IPS7100][:pm0_3]) < Npoints
+        push!(sensors[:IPS7100][:pm0_3], parse(Float64, res["pm0_3"]))
+    else
+        sensors[:IPS7100][:pm0_3] .= circshift(sensors[:IPS7100][:pm0_3], -1)
+        sensors[:IPS7100][:pm0_3][end] = parse(Float64, res["pm0_3"])
+    end
+
+
+    if length(sensors[:IPS7100][:pm0_5]) < Npoints
+        push!(sensors[:IPS7100][:pm0_5], parse(Float64, res["pm0_5"]))
+    else
+        sensors[:IPS7100][:pm0_5] .= circshift(sensors[:IPS7100][:pm0_5], -1)
+        sensors[:IPS7100][:pm0_5][end] = parse(Float64, res["pm0_5"])
+    end
+
+
+    if length(sensors[:IPS7100][:pm1_0]) < Npoints
+        push!(sensors[:IPS7100][:pm1_0], parse(Float64, res["pm1_0"]))
+    else
+        sensors[:IPS7100][:pm1_0] .= circshift(sensors[:IPS7100][:pm1_0], -1)
+        sensors[:IPS7100][:pm1_0][end] = parse(Float64, res["pm1_0"])
+    end
+
+
+    if length(sensors[:IPS7100][:pm2_5]) < Npoints
+        push!(sensors[:IPS7100][:pm2_5], parse(Float64, res["pm2_5"]))
+    else
+        sensors[:IPS7100][:pm2_5] .= circshift(sensors[:IPS7100][:pm2_5], -1)
+        sensors[:IPS7100][:pm2_5][end] = parse(Float64, res["pm2_5"])
+    end
+
+
+    if length(sensors[:IPS7100][:pm10_0]) < Npoints
+        push!(sensors[:IPS7100][:pm10_0], parse(Float64, res["pm10_0"]))
+    else
+        sensors[:IPS7100][:pm10_0] .= circshift(sensors[:IPS7100][:pm10_0], -1)
+        sensors[:IPS7100][:pm10_0][end] = parse(Float64, res["pm10_0"])
+    end
+
+
+
+
+
+
+    bins = parse.(Int,[
+                        res["pc0_1"],
+                        res["pc0_3"],
+                        res["pc0_5"],
+                        res["pc1_0"],
+                        res["pc2_5"],
+                        res["pc5_0"],
+                        res["pc10_0"],
+    ])
+
+
+    if length(sensors[:IPS7100][:bins]) < Npoints
+        push!(sensors[:IPS7100][:bins], bins)
+    else
+        sensors[:IPS7100][:bins] .= circshift(sensors[:IPS7100][:bins], -1)
+        sensors[:IPS7100][:bins][end] = bins
+    end
+
+
+
+end
+
+
+
+function APDS9002()
+    res = JSON.parsefile(sensors[:APDS9002][:path])
+
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:APDS9002][:dt]) > 0 &&  dt == sensors[:APDS9002][:dt][end]
+        return -1
+    end
+
+
+
+    if length(sensors[:APDS9002][:luminance]) < Npoints
+        push!(sensors[:APDS9002][:luminance], parse(Float64, res["luminance"]))
+    else
+        sensors[:APDS9002][:luminance] .= circshift(sensors[:APDS9002][:luminance], -1)
+        sensors[:APDS9002][:luminance][end] = parse(Float64, res["luminance"])
+    end
+
+    if length(sensors[:APDS9002][:dt]) < Npoints
+        push!(sensors[:APDS9002][:dt], dt)
+    else
+        sensors[:APDS9002][:dt] .= circshift(sensors[:APDS9002][:dt], -1)
+        sensors[:APDS9002][:dt][end] = dt
+    end
+end
+
+
+function BME280()
+    res = JSON.parsefile(sensors[:BME280][:path])
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:BME280][:dt]) > 0 dt == sensors[:BME280][:dt][end]
+        return -1
+    end
+
+
+
+
+    if length(sensors[:BME280][:pressure]) < Npoints
+        push!(sensors[:BME280][:pressure], parse(Float64, res["pressure"]))
+    else
+        sensors[:BME280][:pressure] .= circshift(sensors[:BME280][:pressure], -1)
+        sensors[:BME280][:pressure][end] = parse(Float64, res["pressure"])
+    end
+
+
+    if length(sensors[:BME280][:humidity]) < Npoints
+        push!(sensors[:BME280][:humidity], parse(Float64, res["humidity"]))
+    else
+        sensors[:BME280][:humidity] .= circshift(sensors[:BME280][:humidity], -1)
+        sensors[:BME280][:humidity][end] = parse(Float64, res["humidity"])
+    end
+
+
+    if length(sensors[:BME280][:temperature]) < Npoints
+        push!(sensors[:BME280][:temperature], parse(Float64, res["temperature"]))
+    else
+        sensors[:BME280][:temperature] .= circshift(sensors[:BME280][:temperature], -1)
+        sensors[:BME280][:temperature][end] = parse(Float64, res["temperature"])
+    end
+
+
+
+    if length(sensors[:BME280][:dt]) < Npoints
+        push!(sensors[:BME280][:dt], dt)
+    else
+        sensors[:BME280][:dt] .= circshift(sensors[:BME280][:dt], -1)
+        sensors[:BME280][:dt][end] = dt
+    end
+
+end
+
+
+function GUV001()
+    res = JSON.parsefile(sensors[:GUV001][:path])
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:GUV001][:dt]) > 0 && dt == sensors[:GUV001][:dt][end]
+        return -1
+    end
+
+
+
+    if length(sensors[:GUV001][:uvLevel]) < Npoints
+        push!(sensors[:GUV001][:uvLevel], parse(Float64, res["uvLevel"]))
+    else
+        sensors[:GUV001][:uvLevel] .= circshift(sensors[:GUV001][:uvLevel], -1)
+        sensors[:GUV001][:uvLevel][end] = parse(Float64, res["uvLevel"])
+    end
+
+    if length(sensors[:GUV001][:dt]) < Npoints
+        push!(sensors[:GUV001][:dt], dt)
+    else
+        sensors[:GUV001][:dt] .= circshift(sensors[:GUV001][:dt], -1)
+        sensors[:GUV001][:dt][end] = dt
+    end
+end
+
+
+function MGS001()
+    res = JSON.parsefile(sensors[:MGS001][:path])
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:MGS001][:dt]) > 0 && dt == sensors[:MGS001][:dt][end]
+        return -1
+    end
+
+
+
+    if length(sensors[:MGS001][:dt]) < Npoints
+        push!(sensors[:MGS001][:dt], dt)
+    else
+        sensors[:MGS001][:dt] .= circshift(sensors[:MGS001][:dt], -1)
+        sensors[:MGS001][:dt][end] = dt
+    end
+
+    if length(sensors[:MGS001][:c2h5oh]) < Npoints
+        push!(sensors[:MGS001][:c2h5oh], parse(Float64, res["c2h5oh  "]))
+    else
+        sensors[:MGS001][:c2h5oh] .= circshift(sensors[:MGS001][:c2h5oh], -1)
+        sensors[:MGS001][:c2h5oh][end] = parse(Float64, res["c2h5oh  "])
+    end
+
+
+    if length(sensors[:MGS001][:nh3]) < Npoints
+        push!(sensors[:MGS001][:nh3], parse(Float64, res["nh3"]))
+    else
+        sensors[:MGS001][:nh3] .= circshift(sensors[:MGS001][:nh3], -1)
+        sensors[:MGS001][:nh3][end] = parse(Float64, res["nh3"])
+    end
+
+
+    if length(sensors[:MGS001][:c4h10]) < Npoints
+        push!(sensors[:MGS001][:c4h10], parse(Float64, res["c4h10"]))
+    else
+        sensors[:MGS001][:c4h10] .= circshift(sensors[:MGS001][:c4h10], -1)
+        sensors[:MGS001][:c4h10][end] = parse(Float64, res["c4h10"])
+    end
+
+
+    if length(sensors[:MGS001][:c3h8]) < Npoints
+        push!(sensors[:MGS001][:c3h8], parse(Float64, res["c3h8"]))
+    else
+        sensors[:MGS001][:c3h8] .= circshift(sensors[:MGS001][:c3h8], -1)
+        sensors[:MGS001][:c3h8][end] = parse(Float64, res["c3h8"])
+    end
+
+
+    if length(sensors[:MGS001][:ch4]) < Npoints
+        push!(sensors[:MGS001][:ch4], parse(Float64, res["ch4"]))
+    else
+        sensors[:MGS001][:ch4] .= circshift(sensors[:MGS001][:ch4], -1)
+        sensors[:MGS001][:ch4][end] = parse(Float64, res["ch4"])
+    end
+
+
+    if length(sensors[:MGS001][:co]) < Npoints
+        push!(sensors[:MGS001][:co], parse(Float64, res["co"]))
+    else
+        sensors[:MGS001][:co] .= circshift(sensors[:MGS001][:co], -1)
+        sensors[:MGS001][:co][end] = parse(Float64, res["co"])
+    end
+
+
+    if length(sensors[:MGS001][:no2]) < Npoints
+        push!(sensors[:MGS001][:no2], parse(Float64, res["no2"]))
+    else
+        sensors[:MGS001][:no2] .= circshift(sensors[:MGS001][:no2], -1)
+        sensors[:MGS001][:no2][end] = parse(Float64, res["no2"])
+    end
+end
+
+
+function OPCN3()
+    res = JSON.parsefile(sensors[:OPCN3][:path])
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:OPCN3][:dt]) > 0 && dt == sensors[:OPCN3][:dt][end]
+        return -1
+    end
+
+
+    if length(sensors[:OPCN3][:dt]) < Npoints
+        push!(sensors[:OPCN3][:dt], dt)
+    else
+        sensors[:OPCN3][:dt] .= circshift(sensors[:OPCN3][:dt], -1)
+        sensors[:OPCN3][:dt][end] = dt
+    end
+
+    if length(sensors[:OPCN3][:pm1]) < Npoints
+        push!(sensors[:OPCN3][:pm1], parse(Float64, res["pm1"]))
+    else
+        sensors[:OPCN3][:pm1] .= circshift(sensors[:OPCN3][:pm1], -1)
+        sensors[:OPCN3][:pm1][end] = parse(Float64, res["pm1"])
+    end
+
+
+    if length(sensors[:OPCN3][:pm2_5]) < Npoints
+        push!(sensors[:OPCN3][:pm2_5], parse(Float64, res["pm2_5"]))
+    else
+        sensors[:OPCN3][:pm2_5] .= circshift(sensors[:OPCN3][:pm2_5], -1)
+        sensors[:OPCN3][:pm2_5][end] = parse(Float64, res["pm2_5"])
+    end
+
+
+    if length(sensors[:OPCN3][:pm10]) < Npoints
+        push!(sensors[:OPCN3][:pm10], parse(Float64, res["pm10"]))
+    else
+        sensors[:OPCN3][:pm10] .= circshift(sensors[:OPCN3][:pm10], -1)
+        sensors[:OPCN3][:pm10][end] = parse(Float64, res["pm10"])
+    end
+
+
+    bins = parse.(Float64,[
+                            res["pm1"],
+                            res["pm2_5"],
+                            res["pm10"],
+                            res["binCount0"],
+                            res["binCount1"],
+                            res["binCount2"],
+                            res["binCount3"],
+                            res["binCount4"],
+                            res["binCount5"],
+                            res["binCount6"],
+                            res["binCount7"],
+                            res["binCount8"],
+                            res["binCount9"],
+                            res["binCount10"],
+                            res["binCount11"],
+                            res["binCount12"],
+                            res["binCount13"],
+                            res["binCount14"],
+                            res["binCount15"],
+                            res["binCount16"],
+                            res["binCount17"],
+                            res["binCount18"],
+                            res["binCount19"],
+                            res["binCount20"],
+                            res["binCount21"],
+                            res["binCount22"],
+                            res["binCount23"],
+    ])
+
+
+    if length(sensors[:OPCN3][:bins]) < Npoints
+        push!(sensors[:OPCN3][:bins], bins)
+    else
+        sensors[:OPCN3][:bins] .= circshift(sensors[:OPCN3][:bins], -1)
+        sensors[:OPCN3][:bins][end] = bins
+    end
+
+end
+
+
+
+function SI114X()
+    res = JSON.parsefile(sensors[:SI114X][:path])
+    datetime = res["dateTime"]
+    dt = split(datetime, ".")[1]
+    dt = DateTime(dt, dateformat)
+
+    if length(sensors[:SI114X][:dt]) > 0 && dt == sensors[:SI114X][:dt][end]
+        return -1
+    end
+
+
+
+    if length(sensors[:SI114X][:dt]) < Npoints
+        push!(sensors[:SI114X][:dt], dt)
+    else
+        sensors[:SI114X][:dt] .= circshift(sensors[:SI114X][:dt], -1)
+        sensors[:SI114X][:dt][end] = dt
+    end
+
+    if length(sensors[:SI114X][:visible]) < Npoints
+        push!(sensors[:SI114X][:visible], parse(Float64, res["visible"]))
+    else
+        sensors[:SI114X][:visible] .= circshift(sensors[:SI114X][:visible], -1)
+        sensors[:SI114X][:visible][end] = parse(Float64, res["visible"])
+    end
+
+    if length(sensors[:SI114X][:uv]) < Npoints
+        push!(sensors[:SI114X][:uv], parse(Float64, res["uv"]))
+    else
+        sensors[:SI114X][:uv] .= circshift(sensors[:SI114X][:uv], -1)
+        sensors[:SI114X][:uv][end] = parse(Float64, res["uv"])
+    end
+
+    if length(sensors[:SI114X][:ir]) < Npoints
+        push!(sensors[:SI114X][:ir], parse(Float64, res["ir"]))
+    else
+        sensors[:SI114X][:ir] .= circshift(sensors[:SI114X][:ir], -1)
+        sensors[:SI114X][:ir][end] = parse(Float64, res["ir"])
+    end
+
+    if length(sensors[:SI114X][:proximity1]) < Npoints
+        push!(sensors[:SI114X][:proximity1], parse(Float64, res["proximity1"]))
+    else
+        sensors[:SI114X][:proximity1] .= circshift(sensors[:SI114X][:proximity1], -1)
+        sensors[:SI114X][:proximity1][end] = parse(Float64, res["proximity1"])
+    end
+
+    if length(sensors[:SI114X][:proximity2]) < Npoints
+        push!(sensors[:SI114X][:proximity2], parse(Float64, res["proximity2"]))
+    else
+        sensors[:SI114X][:proximity2] .= circshift(sensors[:SI114X][:proximity2], -1)
+        sensors[:SI114X][:proximity2][end] = parse(Float64, res["proximity2"])
+    end
+
+    if length(sensors[:SI114X][:proximity3]) < Npoints
+        push!(sensors[:SI114X][:proximity3], parse(Float64, res["proximity3"]))
+    else
+        sensors[:SI114X][:proximity3] .= circshift(sensors[:SI114X][:proximity3], -1)
+        sensors[:SI114X][:proximity3][end] = parse(Float64, res["proximity3"])
+    end
+end
+
